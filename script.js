007d0df43e457675665e874ff3f27aa788ef7d2b
Ankit07kumar11 <ankitkumar56074@gmail.com>
2023-06-08 23:40:30 +0530

    comments added for better code reading.

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ function allowDrop(ev) {
   ev.preventDefault();
 }
 
+// to drag any element from 1st container
 function drag(ev) {
   ev.dataTransfer.setData("text", ev.target.id);
 }
 
+// function to drop attributes from 1st container to 2nd container
 function drop(ev) {
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
@@ -13,6 +15,7 @@ function drop(ev) {
   alert("Item dropped to div2 successfully");
 }
 
+// function to reset content of 2nd container by appending them back to container 1.
 function resetContainers() {
   var div1 = document.getElementById("div1");
   var div2 = document.getElementById("div2");
